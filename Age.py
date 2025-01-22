f2dfa01346e7aa32cc69536af7065776dbb6bddf
Rozy786-lab <i.rozykul@op.iitg.ac.in>
2025-01-22 12:32:29 +0530

    Update Age.py

--- Age.py
+++ Age.py
@@ -7,7 +7,7 @@ birth_date  = int(input("Enter Date(1-31): "))
 today = datetime.now()
 #Get Current Age
 age = today.year - birth_year
-#Conditioning if the birthday has already passed
+# Conditioning if the birthday hasn't occurred adjust age
 if(today.month, today.day) < (birth_month, birth_date):
     age -=1
     
@@ -24,6 +24,6 @@ next_birthday = datetime(current_year,birth_month,birth_date)
 remaining_days = (next_birthday - today).days
 #Printing the statement
 print(f"Upcoming Birthday: {next_birthday.strftime('%Y-%m-%d')}")
-print(f"Remaining Days:{remaining_days}")
+
    
    
