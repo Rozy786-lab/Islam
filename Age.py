dbfed3f847b05ce6e090ba1ab09cf5ac8f56354c
Rozy786-lab <i.rozykul@op.iitg.ac.in>
2025-01-22 12:27:10 +0530

    Update Age.py

--- Age.py
+++ Age.py
@@ -7,7 +7,7 @@ birth_date  = int(input("Enter Date(1-31): "))
 today = datetime.now()
 #Get Current Age
 age = today.year - birth_year
-#Conditioning if the birthday hasn't occured adjust age
+#Conditioning if the birthday hasn't occurred adjust age
 if(today.month, today.day) < (birth_month, birth_date):
     age -=1
     
